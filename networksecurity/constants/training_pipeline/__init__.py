943d7f5a30ea1247af7b1f913cd0154ddac60532
Atmakuri Vijayavarshini <varshini>
2025-02-03 14:02:19 +0530

    Data Validation Completed

--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -16,9 +16,20 @@ FILE_NAME: str ="phisingData.csv"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 
+SCHEMA_FILE_PATH = os.path.join("data schema", "schema.yaml")
+
 DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"
 DATA_INGESTION_DATABASE_NAME: str = "VARSHINI"
 DATA_INGESTION_DIR_NAME: str = "data_ingenstion"
 DATA_INGESTION_FEATURE_STORE_DIR: str ="feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+
+"""
+Data Validation related constant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report_yaml"
--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import os
-from networksecurity.constant import training_pipeline
+from networksecurity.constants import training_pipeline
 
 print(training_pipeline.PIPELINE_NAME)
 print(training_pipeline.ARTIFACT_DIR)
