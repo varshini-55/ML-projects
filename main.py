c53806adadee2e0347490c3a269e684f0aee5ef3
Atmakuri Vijayavarshini <varshini>
2025-02-05 19:32:38 +0530

    pipeline completed

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig 
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainingCongig
 
 import sys
 
@@ -29,8 +31,9 @@ if __name__=='__main__':
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("data transformation completed")
+
         logging.info("Model Training started")
-        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer_config=ModelTrainingCongig(trainingpipelineconfig)
         model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
         model_trainer_artifact=model_trainer.initiate_model_trainer()
         logging.info("Model Training artifact created")
